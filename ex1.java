5310da916fd1ea4c218de4996d784f1e29cbeb7b
lusida0131 <78531061+lusida0131@users.noreply.github.com>
2021-02-18 18:00:07 +0900

    gukbi support day3

---
+++ ex1.java
@@ -0,0 +1,12 @@
+package example;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		byte n1 =7; //0111
+		byte r1 = (byte)(~n1+1); //1001
+		System.out.println(r1); //1001
+		
+	}
+
+}
